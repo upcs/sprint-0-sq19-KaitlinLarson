ac492c20bacff345a5d21fe980d24e8e38930200
KaitlinLarson <larsonk20@up.edu>
2019-01-29 08:35:20 -0800

    Added a fail test

--- js/functions.test.js
+++ js/functions.test.js
@@ -7,3 +7,7 @@ test('adds 7 + -8 to equal -1', () => {
 test('adds 23 + 22 to equal 45', () => {
 	expect(functions.add(23, 22)).toBe(45);
 });
+
+test('add -1 + -2 to equal 3', () => {
+	expect(functions.add(-1,-2)).toBe(3);
+});
